49d20ebfb94f09c3adddc5d8ee46340f179c3501
Shupermokey <a.e.sobieraj@gmail.com>
2020-11-11 17:47:56 -0600

    Using Cucumber/Gherkin to check if FizzBuzz works

---
+++ lambda-java-example/src/test/java/com/techprimers/testing/FizzBuzzStepDef.java
@@ -0,0 +1,34 @@
+package com.techprimers.testing;
+
+import org.junit.jupiter.api.Assertions;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+
+public class FizzBuzzStepDef {
+ 
+	FizzBuzz fizzBuzz;
+	String result;
+	
+	@Given("^Create a FizzBuzz game play$")
+	public void createAFizzBuzzGamePlay() throws Throwable{
+		fizzBuzz = new FizzBuzz();
+		
+	}
+	
+	@When("^I play with number (\\d+)$")
+	public void iPlayWithNumber(int number) throws Throwable{
+		result = fizzBuzz.play(number);
+		
+	}
+	
+	@Then("^The result is \"([^\"]*)\"$")
+	public void theResultIs(String resultString) throws Throwable{
+		Assertions.assertEquals(result,resultString);
+	}
+	
+
+}
