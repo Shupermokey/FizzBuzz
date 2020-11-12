f86886c8e404f9e0e7fb12a3d713017b8a81e52b
Shupermokey <a.e.sobieraj@gmail.com>
2020-11-12 01:35:14 -0600

    Created a LoginPage for Page Object Model 	-added username, pass, login, and logout elements to the POM Created a copy of the firefox login step definitions and refactored the '@When user enters thier...' to use the LoginPage class

---
+++ lambda-java-example/src/test/java/StepDefinitions/LoginDemoStepDefFireFox_POM.java
@@ -0,0 +1,59 @@
+package StepDefinitions;
+
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import Pages.LoginPage;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class LoginDemoStepDefFireFox_POM {
+
+	WebDriver driver = null;
+	LoginPage loginPage;
+	
+	
+	@Given("^Browser is open$")
+	public void browser_is_open() throws Throwable {
+	    System.out.println("Inside Step - browser is open");
+	    String projectPath = System.getProperty("user.dir");
+	    System.out.println("Project path is: "+projectPath);
+	    
+	    System.setProperty("webdriver.gecko.driver",projectPath+"/src/test/java/Drivers/geckodriver.exe");
+	    driver = new FirefoxDriver();
+	    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	    driver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
+	}
+
+	@Given("^user is on the login page$")
+	public void user_is_on_the_login_page() throws Throwable {
+		System.out.println("Inside Step - user is on the login page");
+		driver.navigate().to("https://example.testproject.io/web/");
+		
+		
+	}
+
+	@When("^user enters their (.*) and (.*)$")
+	public void user_enters_their_username_and_password(String username, String password) throws Throwable {
+		System.out.println("Inside Step - user enters their credentials");
+		loginPage = new LoginPage(driver);
+		loginPage.enterUsername(username);
+		loginPage.enterPassword(password);
+		Thread.sleep(2000);
+		loginPage.clickLogin();
+	}
+
+	@Then("^the user is navigated to the home page$")
+	public void the_user_is_navigated_to_the_home_page() throws Throwable {
+		System.out.println("Inside Step - user is navigated to the home page");
+		loginPage.checkLogOutIsDisplayed();
+		Thread.sleep(2000);
+		driver.close();
+		driver.quit();
+	}
+	
+}
